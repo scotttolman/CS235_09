0f529f6dcf2ab3b133836e03dc8603dd225cf6b9
scotttolman <tol16014@byui.edu>
2017-11-10 16:37:17 -0600

    update

--- assignment09.cpp
+++ assignment09.cpp
@@ -28,10 +28,10 @@ void testBalance();
 // I suggest first commenting out all of these tests, then try to use only
 // TEST1.  Then, when TEST1 works, try TEST2 and so on.
 #define TEST1   // for testSimple()
-#define TEST2   // for testAdd()
-#define TEST3   // for testIterate()
-#define TEST4   // for testDelete()
-#define TESTA   // for testBalance()
+//#define TEST2   // for testAdd()
+//#define TEST3   // for testIterate()
+//#define TEST4   // for testDelete()
+//#define TESTA   // for testBalance()
 
 /**********************************************************************
  * MAIN
